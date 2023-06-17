f95ba2cc4393a8e49dfddd69be4146f641cf4a5c
Alena <weaterwind@gmail.com>
2023-06-17 11:31:08 +0700

    Edit task 30

--- task30.py
+++ task30.py
@@ -4,10 +4,19 @@
 # получения n-го члена прогрессии: an = a1 + (n-1) * d.
 # Каждое число вводится с новой строки.
 
+# list_1 = list()
+# list_1.append(int(input('First element: ')))
+# diff = int(input('Difference between elements: '))
+# number = int(input('Number of elements: '))
+# for i in range(2, number):
+#     list_1.append(list_1[0]+(i-1)*diff)
+# print(*list_1)
+
+## 2 вариант
 list_1 = list()
-list_1.append(int(input('First element: ')))
+first_element = int(input('First element: '))
 diff = int(input('Difference between elements: '))
 number = int(input('Number of elements: '))
-for i in range(2, number):
-    list_1.append(list_1[0]+(i-1)*diff)
+for i in range(first_element, number*diff+first_element, diff):
+    list_1.append(i)
 print(*list_1)
